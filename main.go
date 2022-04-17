9a1208eb9edaf90aa58bccc39dc2b5cd6688b379
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-17 12:25:50 +0900

    Add alias for Single Responsibility

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	iterator "github.com/MatsuoTakuro/design-patterns-in-go/iterator"
+	// singleResp "github.com/MatsuoTakuro/design-patterns-in-go/solid_design_principles/single_responsibility"
 )
 
 func main() {
