86c22701c584e09cbf7431cf5715bb6064cc6239
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-20 20:25:24 +0900

    Adhere to DIP: depend on interfaces, not concrete types

---
+++ singleton/database.go
@@ -0,0 +1,13 @@
+package singleton
+
+type Database interface {
+	GetPopulation(name string) int
+}
+
+func GetTotalPopulationEx(db Database, cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += db.GetPopulation(city)
+	}
+	return result
+}
--- singleton/sub_test.go
+++ singleton/sub_test.go
@@ -7,10 +7,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type DummyDatabase struct {
+	dummyData map[string]int
+}
+
+func (d *DummyDatabase) GetPopulation(name string) int {
+	if len(d.dummyData) == 0 {
+		d.dummyData = map[string]int{
+			"alpha": 1,
+			"beta":  2,
+			"gamma": 3}
+	}
+	return d.dummyData[name]
+}
+
 func TestSub(t *testing.T) {
 	cities := []string{"Seoul", "Mexico City"}
 	tp := GetTotalPopulation(cities)
 	ok := (tp == (17500000 + 17400000))
 	msg := fmt.Sprintf("The total population of Seoul and Mexico City is %d", tp)
+	assert.Equal(t, true, ok, msg)
+
+	names := []string{"alpha", "gamma"}
+	tp = GetTotalPopulationEx(&DummyDatabase{}, names)
+	ok = tp == 4
+	msg = fmt.Sprintf("The total population of alpha and gamma is %d", tp)
 	assert.Equal(t, true, ok, msg)
 }
