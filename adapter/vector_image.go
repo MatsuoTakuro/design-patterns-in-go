4553666a91f9c6c236e8d5b063f393bdd6de2533
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-24 12:20:59 +0900

    Remove redunadant Lines declared

--- adapter/vector_image.go
+++ adapter/vector_image.go
@@ -12,9 +12,9 @@ func NewRectangle(width, height int) *VectorImage {
 	width -= 1
 	height -= 1
 	return &VectorImage{[]Line{
-		Line{0, 0, width, 0},           // bottom
-		Line{0, 0, 0, height},          // left
-		Line{width, 0, width, height},  // right
-		Line{0, height, width, height}, // top
+		{0, 0, width, 0},           // bottom
+		{0, 0, 0, height},          // left
+		{width, 0, width, height},  // right
+		{0, height, width, height}, // top
 	}}
 }
